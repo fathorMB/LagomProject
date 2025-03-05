22c36f456e6e3829ecc36146c2ae34003c0777c0
Moreno BRUSCHI <moreno.bruschi@live.it>
2025-03-06 00:23:47 +0100

    This is a test for workflow triggers (FE)

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,7 @@ import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 
-//Trigger FE pipeline13
+//Trigger FE pipeline14
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
