4768ee293b8d69e4056eb1327dc45b660e8c3245
Moreno BRUSCHI <moreno.bruschi@live.it>
2025-03-06 00:36:37 +0100

    GetIDToken for workflow

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,7 @@ import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 
-//Trigger FE pipeline14
+//Trigger FE pipeline15
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
