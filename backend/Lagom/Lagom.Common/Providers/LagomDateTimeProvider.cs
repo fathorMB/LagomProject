2cf01fcfba41ded36ee9f1b9ae8aee0feca64da8
fathor <13238241+fathorMB@users.noreply.github.com>
2025-03-06 23:01:23 +0100

    LagomDateTimeProvider with offset setting
    * LagomDateTimeProvider with offset setting
    
    * Test for staging env parameter

---
+++ backend/Lagom/Lagom.Common/Providers/LagomDateTimeProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagom.Common.Providers
+{
+    public class LagomDateTimeProvider : ILagomDateTimeProvider
+    {
+        private readonly AppSettings _appSettings;
+
+        public LagomDateTimeProvider(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public DateTime Now => DateTime.Now.AddHours(_appSettings.DateTimeProviderHoursOffset);
+    }
+}
