22da2e144029349cb68c4fe582fb5c7b081f5c9e
fathor <13238241+fathorMB@users.noreply.github.com>
2025-03-07 19:14:16 +0100

    Contact list API
    * Contacts API
    
    * Removed unused code
    
    * Removed useless workflow

---
+++ backend/Lagom/Lagom.Model/Contact.cs
@@ -0,0 +1,18 @@
+﻿using Lagom.Model.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagom.Model
+{
+    public class Contact : LagomDBEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nick { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
