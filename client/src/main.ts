ec1b0a38567436cdf05855cae850d484be7a026b
Moreno BRUSCHI <moreno.bruschi@live.it>
2025-03-07 23:44:28 +0100

    TEST staging triggers

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,7 @@ import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 
-//Trigger FE pipeline16
+//Trigger FE pipeline18
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
