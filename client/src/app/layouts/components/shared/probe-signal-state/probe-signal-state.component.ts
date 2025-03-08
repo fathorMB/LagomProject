286252411fdd62c81f128ad3fa3c58658b5d1c69
Teonirvana <matteo.carelli90@gmail.com>
2025-03-08 23:44:40 +0100

    fixed component and added 2 custom events for probe signal received and error signal received

--- client/src/app/layouts/components/shared/probe-signal-state/probe-signal-state.component.ts
+++ client/src/app/layouts/components/shared/probe-signal-state/probe-signal-state.component.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
-import { ProbeSignal } from 'src/app/model/probe-signal';
+import { DatePipe } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ProbeSignal } from 'src/app/models/probe-signal.model';
 import { ProbeService } from 'src/app/services/probe.service';
 
 @Component({
   selector: 'lagom-probe-signal-state',
   standalone: true,
+  providers: [DatePipe],
   imports: [],
   templateUrl: './probe-signal-state.component.html',
   styleUrl: './probe-signal-state.component.scss'
 })
 export class ProbeSignalStateComponent {
-    
   readonly probeSignalStateErrorMessage: string;
-  probeSignalState: string;
+  probeSignalState: string | null;
 
-  constructor(private _probeService: ProbeService) {
+  @Output() probeSignalReceived = new EventEmitter<ProbeSignal>();
+  @Output() probeSignalErrorReceived = new EventEmitter<string>();
+
+  constructor(private _probeService: ProbeService, private _datePipe: DatePipe) {
     this.probeSignalStateErrorMessage = "No signal received";
     this.probeSignalState = this.probeSignalStateErrorMessage;
   }
@@ -22,17 +26,20 @@ export class ProbeSignalStateComponent {
   ngOnInit(): void {
     this._probeService.startConnection().subscribe(() => {
       this._probeService.receiveProbeSignal().subscribe({
-        next: this.recieveProbeSignal,
-        error: this.handleProbeSignalError
+        next: (probeSignal) => this.receiveProbeSignal(probeSignal),
+        error: (error) => this.receiveProbeSignalError(error)
       });
     });
   }
 
-  private recieveProbeSignal(probeSignal: ProbeSignal): void {
-    this.probeSignalState = probeSignal.apiVersion + " - " + probeSignal.serverDateTime;
+  private receiveProbeSignal(probeSignal: ProbeSignal): void {
+    var serverDateTimeFormatted = '(' + this._datePipe.transform(probeSignal.serverDateTime, 'yyyy-MM-dd HH:mm:ss') + ')';
+    this.probeSignalState = 'v' + probeSignal.apiVersion + ' ' + serverDateTimeFormatted;
+    this.probeSignalReceived.emit(probeSignal);
   }
 
-  private handleProbeSignalError(error: string): void {
+  private receiveProbeSignalError(error: string): void {
     this.probeSignalState = this.probeSignalStateErrorMessage;
+    this.probeSignalErrorReceived.emit(error);
   }
 }
--- client/src/app/services/probe.service.ts
+++ client/src/app/services/probe.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import * as signalR from '@microsoft/signalr';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ProbeSignal } from '../model/probe-signal';
+import { ProbeSignal } from '../models/probe-signal.model';
 
 @Injectable({
   providedIn: 'root'
