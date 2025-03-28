ad4d01befca4b8894e3f2d3f262dd9018ad5efc3
Teonirvana <matteo.carelli90@gmail.com>
2025-03-29 00:40:51 +0100

    refactor style and functions

--- client/src/app/services/snack-bar-manager.service.ts
+++ client/src/app/services/snack-bar-manager.service.ts
@@ -1,35 +1,42 @@
 import { inject, Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackBarManagerService {
-  private readonly snackBar: MatSnackBar = inject(MatSnackBar);
+  private readonly snackBar: MatSnackBar = inject(MatSnackBar);  
   private readonly actionButtonText: string = 'Close';
   private readonly successClass: string = 'green-snackbar';
   private readonly warningClass: string = 'yellow-snackbar';
   private readonly errorClass: string = 'red-snackbar';
+  
+  show(message: string, durationInSeconds: number = 3): MatSnackBarRef<TextOnlySnackBar> {
+    return this.snackBar.open(message, this.actionButtonText, {
+      duration: durationInSeconds * 1000,
+      horizontalPosition: 'end'      
+    });
+  }
 
-  showSuccess(message: string, seconds: number = 3): void {
-    this.snackBar.open(message, this.actionButtonText, {
-      duration: seconds * 1000,
+  showSuccess(message: string, durationInSeconds: number = 3): MatSnackBarRef<TextOnlySnackBar> {
+    return this.snackBar.open(message, this.actionButtonText, {
+      duration: durationInSeconds * 1000,
       horizontalPosition: 'end',
       panelClass: [this.successClass]
     });
   }
 
-  showWarning(message: string, seconds: number = 3): void {
-    this.snackBar.open(message, this.actionButtonText, {
-      duration: seconds * 1000,
+  showWarning(message: string, durationInSeconds: number = 3): MatSnackBarRef<TextOnlySnackBar> {
+    return this.snackBar.open(message, this.actionButtonText, {
+      duration: durationInSeconds * 1000,
       horizontalPosition: 'end',
       panelClass: [this.warningClass]
     });
   }
 
-  showError(message: string, seconds: number = 3): void {
-    this.snackBar.open(message, this.actionButtonText, {
-      duration: seconds * 1000,
+  showError(message: string, durationInSeconds: number = 3): MatSnackBarRef<TextOnlySnackBar> {
+    return this.snackBar.open(message, this.actionButtonText, {
+      duration: durationInSeconds * 1000,
       horizontalPosition: 'end',
       panelClass: [this.errorClass]
     });
