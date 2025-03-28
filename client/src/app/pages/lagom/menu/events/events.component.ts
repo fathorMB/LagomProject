81ca70b0901f0cd51ee1ad9f61c59095e6be1e8b
Teonirvana <matteo.carelli90@gmail.com>
2025-03-28 17:25:36 +0100

    calendar view with default locale IT, events view placeholder PT2

---
+++ client/src/app/pages/lagom/menu/events/events.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vex-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './events.component.html',
+  styleUrl: './events.component.scss'
+})
+export class EventsComponent {
+
+}
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(localeIt)
 
 bootstrapApplication(AppComponent, {
-  providers: [...appConfig.providers]
+  providers: [
+    ...appConfig.providers, 
+    { provide: LOCALE_ID, useValue: 'it' }  // Set Italian locale
+  ]
 });
