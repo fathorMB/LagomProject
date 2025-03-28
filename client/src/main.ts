8bd55b80c370726ef7038841324e2a042b9f2d5b
Moreno BRUSCHI <moreno.bruschi@freseniusmedicalcare.com>
2025-03-28 23:11:50 +0100

    Trigger

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,7 @@ import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 
-//Trigger FE pipeline18
+//Trigger FE pipeline19
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
