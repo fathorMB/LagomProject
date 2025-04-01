bbdfebaf68f52324388cf71f830ac244be8af1a1
Moreno BRUSCHI <moreno.bruschi@freseniusmedicalcare.com>
2025-04-01 20:45:38 +0200

    Allowing client url for staging env

--- backend/Lagom/Lagom.WebAPI/Startup/HTTPPipelineStartup.cs
+++ backend/Lagom/Lagom.WebAPI/Startup/HTTPPipelineStartup.cs
@@ -5,27 +5,34 @@
         public static void AddServices(WebApplicationBuilder webApplicationBuilder)
         {
             webApplicationBuilder.Services.AddControllers();
+
             // Add CORS services
+            string clientURLOrigin = webApplicationBuilder.Configuration["ClientURLOrigin"];
+
             webApplicationBuilder.Services.AddCors(options =>
             {
-                options.AddPolicy("AllowSpecificOrigin",
-                    builder => builder.WithOrigins("http://localhost:4200")
-                                      .AllowAnyHeader()
-                                      .AllowAnyMethod());
+                if (!string.IsNullOrEmpty(clientURLOrigin))
+                    options.AddPolicy("AllowSpecificOrigin",
+                        builder => builder.WithOrigins(clientURLOrigin)
+                                          .AllowAnyHeader()
+                                          .AllowAnyMethod());
+                else
+                    throw new Exception("ClientURLOrigin is not set in appsettings.json");
             });
         }
 
         public static void AddDevelopmentServices(WebApplicationBuilder webApplicationBuilder)
         {
             webApplicationBuilder.Services.AddControllers();
+
             // Add CORS services
             webApplicationBuilder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowSpecificOrigin",
                     builder => builder.WithOrigins("http://localhost:4200", "http://localhost:3000")
-                                      .AllowAnyHeader()
-                                      .AllowAnyMethod()
-                                      .AllowCredentials());
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod()
+                                        .AllowCredentials());
             });
         }
     }
