fcd7fd0e2b0b60bc4c87dbb5b717c17f44f50121
Teonirvana <matteo.carelli90@gmail.com>
2025-04-02 13:04:22 +0200

    removed vex layout content to goo full width in pages

--- client/src/app/pages/lagom/menu/events/events.component.ts
+++ client/src/app/pages/lagom/menu/events/events.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { VexPageLayoutHeaderDirective } from "../../../../../@vex/components/vex-page-layout/vex-page-layout-header.directive";
+import { VexPageLayoutComponent } from "../../../../../@vex/components/vex-page-layout/vex-page-layout.component";
+import { VexBreadcrumbsComponent } from "../../../../../@vex/components/vex-breadcrumbs/vex-breadcrumbs.component";
 
 @Component({
   selector: 'vex-events',
   standalone: true,
-  imports: [],
+  imports: [VexPageLayoutHeaderDirective, VexPageLayoutComponent, VexBreadcrumbsComponent],
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss'
 })
